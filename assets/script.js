ac43162da5f8fcd28edd64ea3d8c8bcde596f507
Bahiya68 <bahiya.atouioual@laposte.net>
2024-02-13 10:21:11 +0100

    defilement carrousel

--- assets/script.js
+++ assets/script.js
@@ -20,11 +20,12 @@ const slides = [
   },
 ];
 
+// initialisation de la variable
 let numero = 0;
 
 console.log(slides);
 
-// Affichage des fléches
+// Action Click sur les fléches droite et gauche
 
 document.querySelector(".arrow_right").addEventListener("click", () => {
   ChangeSlide(+1);
@@ -36,21 +37,26 @@ document.querySelector(".arrow_left").addEventListener("click", () => {
   console.log("fléche gauche");
 });
 
-// Affichage du bullets
-
-const dotsDiv = document.querySelector(".dots");
+const dotsDiv = document.querySelector(".dots"); // Affichage du bullets
 
 for (let i = 0; i < slides.length; i++) {
   const bullet = document.createElement("span");
   dotsDiv.appendChild(bullet);
-  bullet.classList.add("dot");
-  //   bullet.classList.add("dot_selected");
+  bullet.classList.add("dot"); // affiche les bullet inactif (vide)
+
+  if (i === 0) {
+    bullet.classList.add("dot_selected");
+  }
+
   console.log(bullet);
 }
 
 // Image et text du carousel
 
 function ChangeSlide(sens) {
+  const dots = document.getElementsByClassName("dot");
+  dots[numero].classList.remove("dot_selected");
+
   numero = numero + sens;
 
   if (numero > slides.length - 1) {
@@ -60,9 +66,12 @@ function ChangeSlide(sens) {
     numero = slides.length - 1;
   }
 
-  const dots = document.getElementsByClassName("dot");
-  dots[numero].classList.add("dot_selected");
+  // affiche les bulles selectionné
+
+  dots[numero].classList.add("dot_selected"); // affichage des bullet actif
   console.log(dots);
+
+  // pour récupérer les Images et texts
 
   document.querySelector(".banner-img").src =
     "./assets/images/slideshow/" + slides[numero]["image"];
